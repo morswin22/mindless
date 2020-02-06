16b2bb5089a256c140d0fd7a5da5c6f1d6b99beb
morswin22 <xmorswinx@gmail.com>
2020-02-06 22:57:54 +0100

    :sparkles: added login logic

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 require("dotenv").config()
 const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 const http = require("http")
 const https = require("https")
 const path = require("path")
+const { User } = require('./models');
 
 const configurations = {
-  development: { hostname: 'localhost', port: 5000, ssl: false },
-  production: { hostname: 'localhost', port: 80, ssl: false },
+  development: { hostname: 'localhost', port: 5000, ssl: false, db: "mongodb://localhost:27017/mindless" },
+  production: { hostname: 'localhost', port: 80, ssl: false, db: "mongodb://localhost:27017/mindless" },
 }
 
 const environment = process.env.NODE_ENV || "production"
 const config = configurations[environment]
 
+mongoose
+  .connect(config.db, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log(`MongoDB is connected on ${config.db}`))
+  .catch(error => console.error(error))
+
 const app = express()
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "public/")))
 app.use("/js/", express.static(path.join(__dirname, "build/")))
+
+app.post("/login", async ({ body: { name, password } }, res) => {
+  const user = await User.findOne({ name }).exec()
+  if (user && user.password === password) {
+    res.status(200).send({ user, timestamp: Date.now() });
+  } else {
+    res.sendStatus(403);
+  }
+})
 
 let server
 if (config.ssl) {
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import p5 from 'p5'; 
+import Login from './components/Login/Login';
+import User from './components/User/User';
 
-new p5(p => {
-  p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight);
-  };
+const logger = new Login(document.querySelector('#login'));
+const user = new User();
 
-  p.windowResized = () => {
-    p.resizeCanvas(p.windowWidth, p.windowHeight);
-  }
+user.login(logger)
+  .then(() => {
 
-  p.draw = () => {
-    p.background(51);
-    p.fill(255);
-    p.rect(p.width/2 - 25, p.height/2 - 25, 50, 50);
-  };
-}, document.querySelector('#p5'));
+    new p5(p => {
+      p.setup = () => {
+        p.createCanvas(p.windowWidth, p.windowHeight);
+      };
+    
+      p.windowResized = () => {
+        p.resizeCanvas(p.windowWidth, p.windowHeight);
+      }
+    
+      p.draw = () => {
+        p.background(51);
+        p.fill(255);
+        p.rect(p.width/2 - 25, p.height/2 - 25, 50, 50);
+      };
+    }, document.querySelector('#p5'));
+
+  })
+  .catch(console.error);
