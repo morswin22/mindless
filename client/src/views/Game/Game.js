a66e072fae65971e1483cf5412d5653557ef1f52
morswin22 <xmorswinx@gmail.com>
2020-02-25 22:50:17 +0100

    :sparkles: added p5

--- client/src/views/Game/Game.js
+++ client/src/views/Game/Game.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { useAuthorization } from 'components/User/User';
+import P5Wrapper from 'react-p5-wrapper';
 
 const Game = () => {
-  /* const { user } = */ useAuthorization(user => user);
+  /* const { loading, user } = */ useAuthorization(user => user);
 
-  return (<></>)
+  return <P5Wrapper sketch={(p) => {
+
+    p.setup = () => {
+      p.createCanvas(p.windowWidth, p.windowHeight);
+    }
+
+    p.windowResized = () => {
+      p.resizeCanvas(p.windowWidth, p.windowHeight)
+    }
+
+    p.draw = () => {
+      p.background(51);
+
+      p.noStroke();
+      p.fill(255);
+      p.rectMode(p.CENTER);
+      p.rect(p.width/2, p.height/2, 50, 50);
+    }
+
+  }} />;
 }
 
 export default Game;
