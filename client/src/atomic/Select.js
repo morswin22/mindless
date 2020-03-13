db05b4d3ab38bb3c5be7356405df5a461a235957
morswin22 <xmorswinx@gmail.com>
2020-03-13 13:40:08 +0100

    :lipstick: updating atomic styles

--- client/src/atomic/Select.js
+++ client/src/atomic/Select.js
@@ -3,8 +3,19 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ROUTES } from 'utils/Routes/Routes';
 
-const Wrapper = styled.div``;
-const Item = styled(Link)``;
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: column;
+`;
+const Item = styled(Link)`
+  margin: 1em;
+  color: #0080ff;
+  text-decoration: none;
+`;
 
 const Select = ({ subviews }) => (
   <Wrapper>
